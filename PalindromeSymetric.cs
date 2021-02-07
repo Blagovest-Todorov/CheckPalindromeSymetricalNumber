b43fad9145c87b85f73aa1258bbb54bdb8831909
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-07 09:46:00 +0200

    Palindrome solved using method , i think better way this way

--- PalindromeSymetric.cs
+++ PalindromeSymetric.cs
@@ -16,32 +16,27 @@ namespace Palindrome
                     break;
                 }
 
+                bool endResult = IsNumPalindrome(inputNum);
+                Console.WriteLine(endResult.ToString().ToLower());
+            }
+
+            static bool IsNumPalindrome(string inputNum)
+            {
                 int lengthArr = inputNum.Length;   //  1125211 // 1221
                 bool isTrue = true; // acept the number is polindrom-symetric
 
                 for (int i = 0; i < lengthArr / 2; i++)
                 {
-                     int currSymbol = inputNum[i]; // curr symbol from the string
+                    int currSymbol = inputNum[i]; // curr symbol from the string
 
-                     //if (lengthArr % 2 != 0)  // odd number Length                     
-                         if (currSymbol != inputNum[inputNum.Length - 1 - i])
-                         {
-                             isTrue = false;
-                             break;
-                         }  
-                         // no need to make check is the array Length is even or odd /
-                      //else //(lengthArr % 2 == 0 )  // even number Length arra to check                     
-                     // the loop steps are absolutei the same for odd Length and Even Length array!!!
+                    if (currSymbol != inputNum[inputNum.Length - 1 - i])
+                    {
+                        isTrue = false;
+                        break;
+                    }
                 }
 
-                if (isTrue)
-                {
-                    Console.WriteLine("true");
-                }
-                else
-                {
-                    Console.WriteLine("false");
-                }
+                return isTrue;
             }
         }
     }
